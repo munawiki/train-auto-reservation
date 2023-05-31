4ffbf2e42f5fb6fb7e5fb69a1a4d3eb90449f24b
munawiki <munawiki@gmail.com>
2023-05-31 23:35:04 +0900

    Add handleReservation function

--- index.ts
+++ index.ts
@@ -19,6 +19,115 @@ const WANT_START_MINUTE = "10"; // 원하는 출발 분 시작
 const WANT_END_TIME = "22"; // 원하는 출발 시간 끝
 const WANT_END_MINUTE = "00"; // 원하는 출발 분 끝
 
+// export const handleReservation = async (
+//   _event: Electron.IpcMainEvent,
+//   {
+//     account,
+//     accountType,
+//     date,
+//     fromStation,
+//     password,
+//     toStation,
+//     fromTime,
+//     toTime
+//   }: ReservationAPIParams
+// ): Promise<void> => {
+//   const browser = await puppeteer.launch({
+//     headless: false,
+//     defaultViewport: {
+//       width: 1280,
+//       height: 720
+//     }
+//   })
+//   const [page] = await browser.pages()
+
+//   // 로그인
+//   await login(page, { accountType, account, password })
+//   await page.waitForNavigation()
+
+//   const mainUrl = page.url()
+
+//   // 팝업창 닫기
+//   const popup = new Promise((x) => page.once('popup', x))
+//   popup.then((popup) => (popup as Page).close())
+
+//   while (true) {
+//     // 옵션 고르기
+//     await selectOptions(page, {
+//       fromStation,
+//       toStation,
+//       date: dayjs(date).format('YYYY.MM.DD'),
+//       hour: Math.floor(dayjs(`${date} ${fromTime}`).hour() / 2) * 2
+//     })
+
+//     while (true) {
+//       await page.waitForNavigation()
+
+//       const rows = await page.$$('table > tbody > tr')
+
+//       const timeCols = await Promise.all(
+//         rows.map(async (v) => [
+//           await v.$eval('td:nth-child(4) > em', (el) => el.textContent),
+//           await v.$eval('td:nth-child(5) > em', (el) => el.textContent)
+//         ])
+//       )
+
+//       const reservableTimeConditions = timeCols.map(([from, to]) => {
+//         const srtFromDateTime = dayjs(`${date} ${from}`)
+//         const srtToDateTime =
+//           to?.split(':')[0] === '00' ? dayjs(`${date} ${to}`).add(1, 'day') : dayjs(`${date} ${to}`)
+
+//         const userFromTime = dayjs(`${date} ${fromTime}`)
+//         const userToTime = dayjs(`${date} ${toTime}`)
+
+//         return srtFromDateTime.isAfter(userFromTime) && srtToDateTime.isBefore(userToTime)
+//       })
+
+//       const reservableRows = rows.filter((_, i) => reservableTimeConditions[i])
+
+//       const reservableEconomySeatsConditions = await Promise.all(
+//         reservableRows.map((v) =>
+//           v.$eval('td:nth-child(7) > a', (el) => el.textContent === '예약하기')
+//         )
+//       )
+
+//       const reservableEconomySeats = reservableRows.filter(
+//         (_, i) => reservableEconomySeatsConditions[i]
+//       )
+
+//       if (reservableEconomySeats.length > 0) {
+//         await reservableEconomySeats[0].$eval('td:nth-child(7) > a', (el) => el.click())
+//         await page.waitForNavigation()
+
+//         const strong = await page.$(
+//           '#wrap > div.container.container-e > div > div.sub_con_area > div.alert_box > strong'
+//         )
+
+//         const isReservation = await strong?.evaluate(
+//           (el) => el.textContent === '10분 내에 결제하지 않으면 예약이 취소됩니다.'
+//         )
+
+//         if (!isReservation) {
+//           page.goto(mainUrl)
+//           await page.waitForNavigation()
+//           break
+//         }
+
+//         const url = page.url()
+//         const pageID = url.split('?')[1].split('=')[1]
+
+//         sendNotification(
+//           `예약 가능한 좌석이 있습니다. 15분 안에 결제하세요. https://etk.srail.kr/hpg/hra/02/selectReservationList.do?pageId=${pageID}`
+//         )
+
+//         return browser.close()
+//       } else {
+//         page.reload()
+//       }
+//     }
+//   }
+// }
+
 (async function () {
   const browser = await puppeteer.launch({
     headless: false,
